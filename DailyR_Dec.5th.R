6a8904d854391e14be3aac1390fcbabb12065e6c
DOKYUNG JANG <131244639+Iluvchopin12@users.noreply.github.com>
2024-12-05 23:41:04 -0600

    Add files via upload

---
+++ DailyR_Dec.5th.R
@@ -0,0 +1,26 @@
+#Daily R Dec. 5th 
+
+filter(flights, !is.na(dep_delay), is.na(arr_delay)) %>%
+  select(dep_time, arr_time, sched_arr_time, dep_delay, arr_delay)
+
+
+flights %>%
+  filter(!is.na(tailnum)) %>%
+  mutate(on_time = !is.na(arr_time) & (arr_delay <= 0)) %>%
+  group_by(tailnum) %>%
+  summarise(on_time = mean(on_time), n = n()) %>%
+  filter(min_rank(on_time) == 1)
+
+
+quantile(count(flights, tailnum)$n)
+
+
+flights %>%
+  filter(!is.na(tailnum), is.na(arr_time) | !is.na(arr_delay)) %>%
+  mutate(on_time = !is.na(arr_time) & (arr_delay <= 0)) %>%
+  group_by(tailnum) %>%
+  summarise(on_time = mean(on_time), n = n()) %>%
+  filter(n >= 20) %>%
+  filter(min_rank(on_time) == 1)
+
+
